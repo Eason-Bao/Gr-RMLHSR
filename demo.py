cfc24d8b3016b6c090bfc6edec8370a13ce920d7
Eason-Bao <134478665+Eason-Bao@users.noreply.github.com>
2024-09-14 11:27:17 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,134 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from examples_AFEW.dataset_Gras import AfewDataset
+from GrNet_GitHub import *
+
+
+class ManiBlock(nn.Module):
+    def __init__(self, in_datadim=400, out_datadim1=300, out_datadim2=100, out_datadim3=150, embeddim=10):
+        super().__init__()
+        self.p = embeddim
+        self.QR = QRComposition()
+        self.ProjMap = Projmap()
+        self.FR1 = FRMap(in_datadim, out_datadim1)
+        self.FR2 = FRMap(out_datadim3, out_datadim2)
+        self.Orth = Orthmap(self.p)
+        self.Pool = ProjPoolLayer()
+
+    def forward(self, x):
+        x = x.to(torch.float32)
+        x = self.Orth(x)
+        x = self.FR1(x) # 400-300
+        x = self.QR(x)
+        x = self.ProjMap(x)# 300-150
+        x = self.Pool(x)
+        x = self.Orth(x)
+        x = self.FR2(x) # 150-100
+        x = self.QR(x) # 100 * 100
+        return x
+
+class GrNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.ManiBlock = ManiBlock()
+        self.fc = nn.Linear(10000, 7)
+
+    def forward(self, x):
+        x = x.to(torch.float32)
+        x = self.ManiBlock(x)
+        x = self.ManiBlock.ProjMap(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+transformed_dataset_train = AfewDataset(train=True)
+dataloader_train = DataLoader(transformed_dataset_train, batch_size=32,
+                    shuffle=True, num_workers=16)
+transformed_dataset_val = AfewDataset(train=False)
+dataloader_val = DataLoader(transformed_dataset_val, batch_size=32,
+                    shuffle=False, num_workers=16)
+
+
+use_cuda = True
+model = GrNet()
+if use_cuda:
+    model = model.cuda()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+# Training
+def train(epoch):
+    global batch_idx, train_acc
+    print('\nEpoch: %d' % epoch)
+    model.train()
+    train_loss = 0
+    correct = 0.0
+    total = 0.0
+    bar = tqdm(enumerate(dataloader_train))
+    for batch_idx, sample_batched in bar:
+        inputs = sample_batched['data']
+        targets = sample_batched['label'].squeeze()
+
+        if use_cuda:
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.data.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum().data.item()
+
+        train_acc = 100. * correct / total
+        bar.set_description('Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (train_loss/(batch_idx+1.0), 100.*correct/total, correct, total))
+
+    return (train_loss/(batch_idx+1), train_acc)
+
+best_acc = 0
+def test(epoch):
+    global batch_idx, test_acc
+    model.eval()
+    test_loss = 0
+    correct = 0.0
+    total = 0.0
+    bar = tqdm(enumerate(dataloader_val))
+    for batch_idx, sample_batched in bar:
+        inputs = sample_batched['data']
+        targets = sample_batched['label'].squeeze()
+
+        if use_cuda:
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        test_loss += loss.data.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum().data.item()
+
+        test_acc = 100. * correct / total
+        bar.set_description('Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    return (test_loss/(batch_idx+1), test_acc)
+
+
+start_epoch = 1
+for epoch in range(start_epoch, start_epoch+500):
+    train_loss, train_acc = train(epoch)
+    test_loss, test_acc = test(epoch)
+
+
+
